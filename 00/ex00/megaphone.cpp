ac466a68f66f13a2daecfcc975a3433cf1f18257
arommers <arommers@student.codam.nl>
2023-09-20 22:10:07 +0200

    changed the space usage and empty check

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,26 +6,27 @@
 /*   By: arommers <arommers@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/11 10:03:07 by arommers      #+#    #+#                 */
-/*   Updated: 2023/09/11 13:38:56 by arommers      ########   odam.nl         */
+/*   Updated: 2023/09/20 22:01:34 by adri          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
+#include <string>
 
 int main(int argc, char **argv)
 {
     int i = 1;
     int j = 0;
 
-    if (argc <= 1)
+    if (argc <= 1 || std::string(argv[i]).empty())
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     while (argv[i])
     {
         j = 0;
         while (argv[i][j])
             std::cout << (char)toupper(argv[i][j++]);
-        std::cout << " ";
+        // std::cout << " ";
         i++;
     }
     std::cout << std::endl;
