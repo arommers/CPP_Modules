ba7018fd674eef8a635daf3a7f05a770e6e1a7ec
Adri Rommers <arommers@f0r6s12.codam.nl>
2023-09-25 10:47:18 +0200

    just megaphone

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: arommers <arommers@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/11 10:03:07 by arommers      #+#    #+#                 */
-/*   Updated: 2023/09/11 13:38:56 by arommers      ########   odam.nl         */
+/*   Updated: 2023/09/25 10:46:43 by arommers      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,20 @@ int main(int argc, char **argv)
     int i = 1;
     int j = 0;
 
-    if (argc <= 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    while (argv[i])
-    {
-        j = 0;
-        while (argv[i][j])
-            std::cout << (char)toupper(argv[i][j++]);
-        std::cout << " ";
+    while (i < argc &&std::string(argv[i]).empty())
         i++;
+    if (i >= argc)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    else
+    {
+        while (argv[i])
+        {
+            j = 0;
+            while (argv[i][j])
+                std::cout << (char)toupper(argv[i][j++]);
+            i++;
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
     return (0);
 }
