f3460e82ea981b6630deec012b165f9f3554c0b2
Adri Rommers <arommers@f0r6s12.codam.nl>
2023-09-25 11:35:47 +0200

    00 eval version

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,7 +6,8 @@
 /*   By: arommers <arommers@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/11 10:03:07 by arommers      #+#    #+#                 */
-/*   Updated: 2023/09/25 10:48:25 by arommers      ########   odam.nl         */
+
+/*   Updated: 2023/09/25 10:46:43 by arommers      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
