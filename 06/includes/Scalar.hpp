f60fe771fdcfacd904712db07132a51aecab166b
arommers <arommers@student.codam.nl>
2023-12-11 17:08:38 +0100

    started 06

---
+++ 06/includes/Scalar.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Scalar.hpp                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: arommers <arommers@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/12/11 13:59:52 by arommers      #+#    #+#                 */
+/*   Updated: 2023/12/11 16:29:09 by arommers      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+/*  A static method is a class member function that belongs to the class itself, rather than to instances of the class.
+    It can be called using the class name, without the need to create an object of the class. */
+
+class ScalarConverter
+{
+    private:
+        ScalarConverter();  
+        ScalarConverter(const ScalarConverter& original);
+        ScalarConverter&    operator=(const ScalarConverter& rhs);
+        ~ScalarConverter();
+    public:
+        static void convert(const std::string input);
+};
---
+++ 06/sources/Scalar.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Scalar.cpp                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: arommers <arommers@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/12/11 14:03:00 by arommers      #+#    #+#                 */
+/*   Updated: 2023/12/11 16:30:47 by arommers      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Scalar.hpp"
+
+ScalarConverter::ScalarConverter() {}
+
+ScalarConverter::ScalarConverter(const ScalarConverter& original) {}
+
+ScalarConverter::operator=(const ScalarConverter& rhs) {}
+
+ScalarConverter::~ScalarConverter() {}
+
+void    Scalar:convert(const std::string input) 
+{
+    
+}
